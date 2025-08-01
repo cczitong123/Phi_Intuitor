199963ca655e121572d61315c6194e9b77c95401
txyang1 <tingxinyang2@gamil.com>
2025-08-01 13:22:51 +0200

    delete reward model or self_certaintay compute and use prm_reward to replace

--- verl-intuitor/verl/trainer/ppo/ray_trainer.py
+++ verl-intuitor/verl/trainer/ppo/ray_trainer.py
@@ -378,7 +378,7 @@ class RayPPOTrainer:
         self.role_worker_mapping = role_worker_mapping
         self.resource_pool_manager = resource_pool_manager
         self.use_reference_policy = Role.RefPolicy in role_worker_mapping
-        self.use_rm = Role.RewardModel in role_worker_mapping
+        #self.use_rm = Role.RewardModel in role_worker_mapping
         self.ray_worker_group_cls = ray_worker_group_cls
         self.device_name = device_name
         self.validation_generations_logger = ValidationGenerationsLogger()
@@ -879,11 +879,11 @@ class RayPPOTrainer:
         else:
             raise NotImplementedError
 
-        # # create critic
-        # if self.use_critic:
-        #     resource_pool = self.resource_pool_manager.get_resource_pool(Role.Critic)
-        #     critic_cls = RayClassWithInitArgs(cls=self.role_worker_mapping[Role.Critic], config=self.config.critic)
-        #     self.resource_pool_to_cls[resource_pool]["critic"] = critic_cls
+        # create critic
+        if self.use_critic:
+            resource_pool = self.resource_pool_manager.get_resource_pool(Role.Critic)
+            critic_cls = RayClassWithInitArgs(cls=self.role_worker_mapping[Role.Critic], config=self.config.critic)
+            self.resource_pool_to_cls[resource_pool]["critic"] = critic_cls
 
         # create reference policy if needed
         if self.use_reference_policy:
@@ -896,12 +896,12 @@ class RayPPOTrainer:
             )
             self.resource_pool_to_cls[resource_pool]["ref"] = ref_policy_cls
 
-        # create a reward model if reward_fn is None
-        if self.use_rm:
-            # we create a RM here
-            resource_pool = self.resource_pool_manager.get_resource_pool(Role.RewardModel)
-            rm_cls = RayClassWithInitArgs(self.role_worker_mapping[Role.RewardModel], config=self.config.reward_model)
-            self.resource_pool_to_cls[resource_pool]["rm"] = rm_cls
+        # # create a reward model if reward_fn is None
+        # if self.use_rm:
+        #     # we create a RM here
+        #     resource_pool = self.resource_pool_manager.get_resource_pool(Role.RewardModel)
+        #     rm_cls = RayClassWithInitArgs(self.role_worker_mapping[Role.RewardModel], config=self.config.reward_model)
+        #     self.resource_pool_to_cls[resource_pool]["rm"] = rm_cls
 
         # initialize WorkerGroup
         # NOTE: if you want to use a different resource pool for each role, which can support different parallel size,
@@ -940,9 +940,9 @@ class RayPPOTrainer:
             self.ref_policy_wg = all_wg["ref"]
             self.ref_policy_wg.init_model()
 
-        if self.use_rm:
-            self.rm_wg = all_wg["rm"]
-            self.rm_wg.init_model()
+        # if self.use_rm:
+        #     self.rm_wg = all_wg["rm"]
+        #     self.rm_wg.init_model()
 
         # we should create rollout at the end so that vllm can have a better estimation of kv cache memory
         self.actor_rollout_wg = all_wg["actor_rollout"]
@@ -1145,8 +1145,8 @@ class RayPPOTrainer:
                         self.ref_policy_wg.start_profile()
                     if self.use_critic:
                         self.critic_wg.start_profile()
-                    if self.use_rm:
-                        self.rm_wg.start_profile()
+                    # if self.use_rm:
+                    #     self.rm_wg.start_profile()
 
                 metrics = {}
                 timing_raw = {}
@@ -1328,12 +1328,12 @@ class RayPPOTrainer:
                             config=self.config.algorithm,
                         )
 
-                    # update critic
-                    if self.use_critic:
-                        with marked_timer("update_critic", timing_raw, color="pink"):
-                            critic_output = self.critic_wg.update_critic(batch)
-                        critic_output_metrics = reduce_metrics(critic_output.meta_info["metrics"])
-                        metrics.update(critic_output_metrics)
+                    # # update critic
+                    # if self.use_critic:
+                    #     with marked_timer("update_critic", timing_raw, color="pink"):
+                    #         critic_output = self.critic_wg.update_critic(batch)
+                    #     critic_output_metrics = reduce_metrics(critic_output.meta_info["metrics"])
+                    #     metrics.update(critic_output_metrics)
 
                     # implement critic warmup
                     if self.config.trainer.critic_warmup <= self.global_steps:
@@ -1426,8 +1426,8 @@ class RayPPOTrainer:
                         self.ref_policy_wg.stop_profile()
                     if self.use_critic:
                         self.critic_wg.stop_profile()
-                    if self.use_rm:
-                        self.rm_wg.stop_profile()
+                    # if self.use_rm:
+                    #     self.rm_wg.stop_profile()
 
                 if is_last_step:
                     pprint(f"Final validation metrics: {last_val_metrics}")
